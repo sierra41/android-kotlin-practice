c7e324f7bcb040d35c3e689c771544a4d97fc54d
sierra41 <actin45@gmail.com>
2021-04-07 16:56:10 +0900

    [CHORE]  androidStudio javaPractive 1 생성

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-##[2021.03.18 ~ 2021.05.09] 
+# android-tpKotlin Android Test Project
 
+# android_tp
+Kotlin 기반 android test project
+
+
+## 참고
+### [안드로이드 앱 개발](https://www.youtube.com/watch?v=WlJszSmK_es)
--- app/src/main/java/com/example/test28/MainActivity.java
+++ app/src/main/java/com/example/test28/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.test28;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        String str  = intent.getExtras().getString("키");
+        //EditText edittext  = findViewById(R.id.edit_text);
+
+        ((EditText) findViewById(R.id.edit_text)).setText(str);
+        Log.d("로그", str);
+
+            // o- cmmit
+
+                //o-master
+
+
+
     }
 
 
