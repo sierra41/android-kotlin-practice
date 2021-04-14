ef12be8f39927a3d2683d1ea6bca50f47e02a424
sierra41 <actin45@gmail.com>
2021-04-14 17:56:10 +0900

    [FEAT]2- kotlin xml설정, drqwable, mipmap 등

--- test21/app/src/main/java/com/example/test21/MainActivity.kt
+++ test21/app/src/main/java/com/example/test21/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.test21
 
 import android.os.Bundle
-import android.widget.Toast
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,11 +11,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)      //java 에서 find viewid 가져오던 것과 다르게 여기서 알아서 찾아줌
 
-        btn.setOnClickListener{
-            Toast.makeText(this, "toast!", Toast.LENGTH_SHORT).show()
-        }                //'.setOnClickListener {
-
+/*       0. kotlin으로 버튼 토스트 btn.setOnClickListener{
+            Toast.makeText(this, "toast!", Toast.LENGTH_SHORT).show() //토스트 띄우기
+        }*/
 
     }
+
+    fun DDuck (v : View){  //view 가져오기
+       /* println("뚝!")*/
+        tv.text = "뚝"  // text 변경
+
+    }
+
 
 }
