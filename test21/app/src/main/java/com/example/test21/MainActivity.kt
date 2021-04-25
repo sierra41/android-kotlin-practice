00572549be829cdd090261c098e44648d1da649a
sierra41 <actin45@gmail.com>
2021-04-25 17:56:10 +0900

    image 설정

--- test21/app/src/main/java/com/example/test21/MainActivity.kt
+++ test21/app/src/main/java/com/example/test21/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.test21
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
+import android.provider.MediaStore
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,11 +12,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)      //java 에서 find viewid 가져오던 것과 다르게 여기서 알아서 찾아줌
 
-        btn.setOnClickListener{
-            Toast.makeText(this, "toast!", Toast.LENGTH_SHORT).show()
-        }                //'.setOnClickListener {
+        /*edittext에 글자를 입력할 때 하나하나 로그에 표시*/
 
 
+      /*  et.addTextChangedListener(object  : TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {
+                println("입력끝 " + p0)
+            }
+            override fun beforeTextChanged(p0: CharSequence?, start: Int, count: Int, after: Int) {
+                println("입력전 " + p0)
+            }
+            override fun onTextChanged(p0: CharSequence?, start: Int, before: Int, count: Int) {
+                println("입력중 " + p0)
+            }
+
+        })*/
+
+    }
+
+    fun LoadImage(v : View){
+        startActivityForResult(Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI), 0)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data : Intent?)
+
+        if(data != null)
     }
 
 }
---
+++ test21/app/src/main/java/com/example/test21/Test2Activity.kt
@@ -0,0 +1,32 @@
+package com.example.test21
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Test2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)      //java 에서 find viewid 가져오던 것과 다르게 여기서 알아서 찾아줌
+
+/*       0. kotlin으로 버튼 토스트 btn.setOnClickListener{
+            Toast.makeText(this, "toast!", Toast.LENGTH_SHORT).show() //토스트 띄우기
+        }*/
+
+        /*20. 버튼 visibility 설정
+       btn.visibility = View.INVISIBLE */
+        /*21. 버튼 enable
+        btn.isEnabled =   false
+        btn.isClickable =   false*/
+
+
+    }
+
+    /*fun DDuck (v : View){  //view 가져오기
+       *//* println("뚝!")*//*
+        tv.text = "뚝"  // text 변경
+
+    }*/
+
+
+}
