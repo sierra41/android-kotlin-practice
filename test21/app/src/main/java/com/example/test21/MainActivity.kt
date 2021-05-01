44e5eacded758b56a0d3964e668ead65282c6486
sierra41 <actin45@gmail.com>
2021-05-01 17:56:10 +0900

    [Feat]3 switch, textinputlayout, cb

--- test21/app/src/main/java/com/example/test21/MainActivity.kt
+++ test21/app/src/main/java/com/example/test21/MainActivity.kt
@@ -1,28 +1,43 @@
 package com.example.test21
 
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager.widget.PagerAdapter
-import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
-    var viewList = ArrayList<View>()
+   // var viewList = ArrayList<View>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewList.add(layoutInflater.inflate(R.layout.fragment_home, null))
+
+        //switch
+
+        /*switch1.setOnCheckedChangeListener {compoundButton, b ->
+            println(b)
+
+        }*/
+
+
+
+        /*viewList.add(layoutInflater.inflate(R.layout.fragment_home, null))
         viewList.add(layoutInflater.inflate(R.layout.fragment_favorite, null))
         viewList.add(layoutInflater.inflate(R.layout.fragment_profile, null))
 
         viewPager.adapter = pagerAdapter()
+
+        tabLayout.setupWithViewPager(viewPager)
+        tabLayout.getTabAt(0)?.setText("home")
+        tabLayout.getTabAt(1)?.setText("Favorit")
+        tabLayout.getTabAt(2)?.setText("Profile")
+        tabLayout.getTabAt(0)?.setIcon(R.drawable.ic_launcher_background)
+        tabLayout.getTabAt(1)?.setIcon(R.drawable.ic_launcher_foreground)
+        tabLayout.getTabAt(2)?.setIcon(R.drawable.ic_launcher_background)*/
+
     }
 
-    inner class pagerAdapter : PagerAdapter() {
+ /*   inner class pagerAdapter : PagerAdapter() {
 
         override fun isViewFromObject(view: View, `object`: Any) = view == 'object'
 
@@ -38,5 +53,5 @@ class MainActivity : AppCompatActivity() {
         override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
             viewPager.removeView('object' as View)
         }
-    }
+    }*/
 }
