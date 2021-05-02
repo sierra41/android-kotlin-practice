27049f3f10983ac48bac7db9ef3847bde784d994
sierra41 <actin45@gmail.com>
2021-05-02 17:56:10 +0900

    [Feat]3 timepicker, audio

--- test21/app/src/main/java/com/example/test21/MainActivity.kt
+++ test21/app/src/main/java/com/example/test21/MainActivity.kt
@@ -5,53 +5,31 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-   // var viewList = ArrayList<View>()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //btn.setOnClickListener{ Vibration(1000L)}
+    }
+    /* 기기 진동
+    fun Vibration(len:Long) {
+        val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
 
-        //switch
+        vibrator.vibrate(len)
 
-        /*switch1.setOnCheckedChangeListener {compoundButton, b ->
-            println(b)
+    }*/
 
-        }*/
+    /*볼륨조절*/
+/*
+    fun Audio(v: View) {
 
-
-
-        /*viewList.add(layoutInflater.inflate(R.layout.fragment_home, null))
-        viewList.add(layoutInflater.inflate(R.layout.fragment_favorite, null))
-        viewList.add(layoutInflater.inflate(R.layout.fragment_profile, null))
-
-        viewPager.adapter = pagerAdapter()
-
-        tabLayout.setupWithViewPager(viewPager)
-        tabLayout.getTabAt(0)?.setText("home")
-        tabLayout.getTabAt(1)?.setText("Favorit")
-        tabLayout.getTabAt(2)?.setText("Profile")
-        tabLayout.getTabAt(0)?.setIcon(R.drawable.ic_launcher_background)
-        tabLayout.getTabAt(1)?.setIcon(R.drawable.ic_launcher_foreground)
-        tabLayout.getTabAt(2)?.setIcon(R.drawable.ic_launcher_background)*/
-
+        var audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
+        audioManager.getStreamMaxVolume(AudioManager.STREAM_VOICE_CALL)
+        audioManager.getStreamMaxVolume(AudioManager.STREAM_RING)
+        audioManager.getStreamMaxVolume(AudioManager.STREAM_ALARM)
     }
 
- /*   inner class pagerAdapter : PagerAdapter() {
+    */
 
-        override fun isViewFromObject(view: View, `object`: Any) = view == 'object'
-
-        override fun getCount() = viewList.size
-
-        override fun instantiateItem(container: ViewGroup, position: Int): Any {
-            var curView = viewList[position]
-            viewPager.addView(curView)
-            return curView
-
-        }
-
-        override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
-            viewPager.removeView('object' as View)
-        }
-    }*/
 }
