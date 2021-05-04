ac9b70b381d7c2bdd17147fd2754610a5146cd68
sierra41 <actin45@gmail.com>
2021-05-04 17:56:10 +0900

    [Feat]3 센서설정, 네트워크설정

--- test21/app/src/main/java/com/example/test21/MainActivity.kt
+++ test21/app/src/main/java/com/example/test21/MainActivity.kt
@@ -1,81 +1,63 @@
 package com.example.test21
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.Context
-import android.content.Intent
-import android.os.Build
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.net.ConnectivityManager
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.NotificationCompat
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    //와이파이데이터체크
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn.setOnClickListener{ NetworkCheck(btn)}
+    }
+
+    fun NetworkCheck(v: View){
+
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        println(connectivityManager.activeNetworkInfo?.isConnected)
+    }
+
+
+    //센서 설정
+    /*lateinit var sensorManager: SensorManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //푸시
-        //btn.setOnClickListener{ ShowNotification("제목", "내용")}
+    }
+    override fun onResume() {
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_NORMAL)
 
-        val pref = getSharedPreferences("pref", Context.MODE_PRIVATE)
-        pref.edit().putString("키", "값").apply()
-        val value = pref.getString("키", "저장안됨")
-        pref.edit().remove("키").apply()
-        pref.edit().clear().apply()
-
-
-        //타임피커
-        //btn.setOnClickListener{ Vibration(1000L)}
-        //timePicker.hour
-        //timePicker.minute
-        /*timePicker.setOnTimeChangedListener{timePicker, i , i2 ->
-
-            println(i.toString() + ": " + i2.toString())
-        }*/
+        super.onResume()
     }
 
-    private fun ShowNotification(Title: String, Body: String) {
-        val pending = PendingIntent.getActivity(this, 0, Intent(this, MainActivityL::class.java), PendingIntent.FLAG_CANCEL_CURRENT)
-        val builder = NotificationCompat.Builder(this, "id")
-        builder.setSmallIcon(R.drawable.ic_launcher_background)
-                .setContentText(Body)
-                .setContentTitle(Title)
-                .setContentIntent(pending)
-                .setAutoCancel(true)
-                .setDefaults(Notification.DEFAULT_SOUND or Notification.DEFAULT_VIBRATE)
+    override fun onPause() {
+        super.onPause()
 
-
-        val NManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.0) {
-            NManager.createNotificationChannel(NotificationChannel("id", "name"), NotificationManager.IMPORTANCE_HIGH))
-
-        }
-        NManager.notify(0, builder.build())
-
+        sensorManager.unregisterListener(this)
     }
-    /* 기기 진동
-    fun Vibration(len:Long) {
-        val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        TODO("Not yet implemented")
+    }
 
-        vibrator.vibrate(len)
+    override fun onSensorChanged(p0: SensorEvent?) {
+        val x = event?.value?.get(0) as Float
+        val z = event?.value?.get(1) as Float
+        val y = event?.value?.get(2) as Float
 
+        println(x.toString() + ", " + y.toString() + ", " + z.toString())
     }*/
 
-    /*볼륨조절*/
-/*
-    fun Audio(v: View) {
-
-        var audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
-        audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
-        audioManager.getStreamMaxVolume(AudioManager.STREAM_VOICE_CALL)
-        audioManager.getStreamMaxVolume(AudioManager.STREAM_RING)
-        audioManager.getStreamMaxVolume(AudioManager.STREAM_ALARM)
-    }
-
-    */
 
 }
