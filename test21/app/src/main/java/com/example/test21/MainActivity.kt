22452def44c90699f2c7a12a5371aa9c0caad1bb
sierra41 <actin45@gmail.com>
2021-05-06 17:56:10 +0900

    [Feat]3 include dial

--- test21/app/src/main/java/com/example/test21/MainActivity.kt
+++ test21/app/src/main/java/com/example/test21/MainActivity.kt
@@ -1,41 +1,47 @@
 package com.example.test21
 
-import android.content.Context
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
-import android.hardware.SensorManager
-import android.media.MediaParser
-import android.net.ConnectivityManager
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 
-class MainActivity : AppCompatActivity(), SensorEventListener {
-
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //btn.setOnClickListener{ Click(btn)}
     }
 
-   /* fun Click(v:View){
-        val mediaPlayer = MediaParser.create(this, R.raw.coin_sound)
-        mediaPlayer.start()
-        mediaPlayer.setOnCompletionListener { mediaPlayer.release()}
-    }*/
+    btn.setOnClickListener {
+     val ad = AlertDialog.Builder(this)
+        ad.setTitle("제목")
+        ad.setMessage("내용")
+        ad.setIcon(R.mipmap.ic_launcher)
+        ad.setView(EditText(this))
+        ad.setPositiveButton("네", object:DialogInterface.OnClickListener{
+            override fun onClick(dialog: DialogInterface?, which: Int) {
+                println("네 클릭")
+                p0?.dismiss()
+            }
+        })
+        ad.setNegativeButton("아니요", object:DialogInterface.OnClickListener{
+            override fun onClick(dialog: DialogInterface?, which: Int) {
+                println("아니오오 클릭"
+                p0?.dismiss()
+            }
+        })
+    }
 
-}
 
-/*클래스로 flag*/
-class OneFragment:Fragment(){
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?)
-        //eturn super.onCreateView(inflater, container, savedInstanceState)
-        = inflater.inflate(R.layout.frag1, container, false)
-
-
+    btn.setOnClickListener
+    {
+        var intent = Intent()
+        intent.setAction(Intent.ACTION_SEND)
+        intent.setType("text/plain")
+        intent.putExtra(Intent.EXTRA_SUBJECT, title)
+        intent.putExtra(Intent.EXTRA_TEXT, "내용")
+        startActivity(Intent.createChooser(intent, "공유"))
+    }
 }
---
+++ test21/app/src/main/java/com/example/test21/test8MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.test21
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.media.MediaParser
+import android.net.ConnectivityManager
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //btn.setOnClickListener{ Click(btn)}
+        btn1.setOnClickListener{ supportFragmentManager.beginTransaction().replace(R.id.frameLayout, OneFragment()).commit()}
+        btn2.setOnClickListener{ supportFragmentManager.beginTransaction().replace(R.id.frameLayout, TwoFragment()).commit()}
+
+    }
+
+   /* fun Click(v:View){
+        val mediaPlayer = MediaParser.create(this, R.raw.coin_sound)
+        mediaPlayer.start()
+        mediaPlayer.setOnCompletionListener { mediaPlayer.release()}
+    }*/
+
+}
+
+/*클래스로 flag*/
+class OneFragment:Fragment(){
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?)
+        //eturn super.onCreateView(inflater, container, savedInstanceState)
+        = inflater.inflate(R.layout.frag1, container, false)
+
+
+}class TwoFragment:Fragment(){
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?)
+    //eturn super.onCreateView(inflater, container, savedInstanceState)
+            = inflater.inflate(R.layout.frag2, container, false)
+
+
+}
