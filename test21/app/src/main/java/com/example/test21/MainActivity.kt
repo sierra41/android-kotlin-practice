13f2a42fa7ccd01701c2f1e3abf64d3faec15991
sierra41 <actin45@gmail.com>
2021-05-07 17:56:10 +0900

    [Feat]3 공유, 애니메이션

--- test21/app/src/main/java/com/example/test21/MainActivity.kt
+++ test21/app/src/main/java/com/example/test21/MainActivity.kt
@@ -6,42 +6,18 @@ import android.os.Bundle
 import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.flag1.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    btn.setOnClickListener {
-     val ad = AlertDialog.Builder(this)
-        ad.setTitle("제목")
-        ad.setMessage("내용")
-        ad.setIcon(R.mipmap.ic_launcher)
-        ad.setView(EditText(this))
-        ad.setPositiveButton("네", object:DialogInterface.OnClickListener{
-            override fun onClick(dialog: DialogInterface?, which: Int) {
-                println("네 클릭")
-                p0?.dismiss()
-            }
-        })
-        ad.setNegativeButton("아니요", object:DialogInterface.OnClickListener{
-            override fun onClick(dialog: DialogInterface?, which: Int) {
-                println("아니오오 클릭"
-                p0?.dismiss()
-            }
-        })
-    }
+        var render = Render(this)
+        render.setAnimation(Bounce().InDown(btn))
+        render.start()
 
-
-    btn.setOnClickListener
-    {
-        var intent = Intent()
-        intent.setAction(Intent.ACTION_SEND)
-        intent.setType("text/plain")
-        intent.putExtra(Intent.EXTRA_SUBJECT, title)
-        intent.putExtra(Intent.EXTRA_TEXT, "내용")
-        startActivity(Intent.createChooser(intent, "공유"))
     }
 }
---
+++ test21/app/src/main/java/com/example/test21/test9MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.test21
+
+import android.content.DialogInterface
+import android.content.Intent
+import android.os.Bundle
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+
+class test9MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+
+    btn.setOnClickListener {
+     val ad = AlertDialog.Builder(this)
+        ad.setTitle("제목")
+        ad.setMessage("내용")
+        ad.setIcon(R.mipmap.ic_launcher)
+        ad.setView(EditText(this))
+        ad.setPositiveButton("네", object:DialogInterface.OnClickListener{
+            override fun onClick(dialog: DialogInterface?, which: Int) {
+                println("네 클릭")
+                p0?.dismiss()
+            }
+        })
+        ad.setNegativeButton("아니요", object:DialogInterface.OnClickListener{
+            override fun onClick(dialog: DialogInterface?, which: Int) {
+                println("아니오오 클릭"
+                p0?.dismiss()
+            }
+        })
+        ad.show()
+    }
+
+
+    btn.setOnClickListener
+    {
+        var intent = Intent()
+        intent.setAction(Intent.ACTION_SEND)
+        intent.setType("text/plain")
+        intent.putExtra(Intent.EXTRA_SUBJECT, title)
+        intent.putExtra(Intent.EXTRA_TEXT, "내용")
+        startActivity(Intent.createChooser(intent, "공유"))
+    }
+}
